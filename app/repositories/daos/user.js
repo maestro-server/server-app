472b31abda12bbdac551302421a5cff8bba70069
Felipe Signorini <felipeklerk@yahoo.com.br>
2017-03-21 19:47:14 -0300

    passhash

--- app/repositories/daos/user.js
+++ app/repositories/daos/user.js
@@ -8,6 +8,16 @@ class UserDao extends Model {
         return 'users';
     }
 
+    configure () {
+        this.before('create', 'passHash');
+    }
+
+    *passHash (next) {
+      console.log(this);
+
+      yield next;
+    }
+
     static isDuplicate (email) {
         return UserDao.where('email', email).findOne();
     }
