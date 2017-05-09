81f16b9f5a0cb92ffab7a0d14125bc865637bb52
Felipe Signorini <felipeklerk@yahoo.com.br>
2017-05-09 12:11:36 -0300

    ajust cors

--- app/middlewares/cors.js
+++ app/middlewares/cors.js
@@ -3,7 +3,7 @@
 module.exports = function () {
     return function (req, res, next) {
       res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
       next();
     };
 };
