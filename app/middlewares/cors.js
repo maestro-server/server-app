60c88c300661f5b94f1b4dd37fb57bc8d2311d59
Felipe Signorini <felipeklerk@yahoo.com.br>
2017-05-15 23:55:43 -0300

    bug add team user

--- app/middlewares/cors.js
+++ app/middlewares/cors.js
@@ -3,7 +3,7 @@
 module.exports = function () {
     return function (req, res, next) {
       res.header("Access-Control-Allow-Origin", "*");
-      res.header('Access-Control-Allow-Methods', "GET,PUT,POST,DELETE");
+      res.header('Access-Control-Allow-Methods', "GET,PUT,POST,DELETE,PATCH");
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
       next();
     };
