b621b440f2d03f16c3a35cad4fdadd698526837a
Felipe Signorini <felipeklerk@yahoo.com.br>
2017-05-29 23:14:05 -0300

    teams testes

--- test/e2e/teams.js
+++ test/e2e/teams.js
@@ -24,6 +24,14 @@ describe('e2e users: user - create, update, delete', function () {
         _id: null
     };
 
+    let friend = {
+        name: "Friend",
+        email: "friend@maestrousers.com",
+        password: "mytester",
+        token: null,
+        _id: null
+    };
+
     let teams = [{
         name: "MyTeam",
         email: "maestro@team.com"
@@ -67,6 +75,23 @@ describe('e2e users: user - create, update, delete', function () {
                     done(err);
                 });
         });
+
+        it('Create account - success friend', function (done) {
+            request(mock)
+                .post('/users')
+                .send(friend)
+                .expect(201)
+                .expect('Content-Type', /json/)
+                .expect(/\"name\":\"Friend\"/)
+                .expect(/_id/)
+                .expect((res) => {
+                    friend._id = res.body._id;
+                })
+                .end(function (err) {
+                    if (err) return done(err);
+                    done(err);
+                });
+        });
     });
 
     describe('e2e teams: get token', function () {
@@ -101,6 +126,17 @@ describe('e2e users: user - create, update, delete', function () {
                 .expect(/MyTeam/)
                 .expect(/maestro@team\.com/)
                 .expect(/_id/)
+                .end(function (err) {
+                    if (err) return done(err);
+                    done(err);
+                });
+        });
+
+        it('Create team - create team without token', function (done) {
+            request(mock)
+                .post('/teams')
+                .send(teams[0])
+                .expect(401)
                 .end(function (err) {
                     if (err) return done(err);
                     done(err);
@@ -203,6 +239,16 @@ describe('e2e users: user - create, update, delete', function () {
                 });
         });
 
+        it('Exist team - see my new team without token', function (done) {
+            request(mock)
+                .get('/teams/'+teams[0]._id)
+                .expect(401)
+                .end(function (err) {
+                    if (err) return done(err);
+                    done(err);
+                });
+        });
+
         it('Exist team -  autocomplete', function (done) {
             request(mock)
                 .get('/teams/autocomplete')
@@ -221,6 +267,170 @@ describe('e2e users: user - create, update, delete', function () {
                     done(err);
                 });
         });
+
+        it('Exist team -  autocomplete without token', function (done) {
+            request(mock)
+                .get('/teams/autocomplete')
+                .query({complete: "second"})
+                .expect(401)
+                .end(function (err) {
+                    if (err) return done(err);
+                    done(err);
+                });
+        });
+    });
+
+
+    describe('e2e teams: update team', function () {
+        it('Exist team - update team with valid data', function (done) {
+            const data = Object.assign(teams[0], {name: "ChangeName", email: "changeemail@email.com", url: "http://changeurl.com.br"});
+
+            request(mock)
+                .patch('/teams/'+teams[0]._id)
+                .send(data)
+                .set('Authorization', `JWT ${user.token}`)
+                .expect(202)
+                .expect('Content-Type', /json/)
+                .expect(/\"name\":\"ChangeName\"/)
+                .expect(/changeemail@email\.com/)
+                .expect(/http:\/\/changeurl\.com\.br/)
+                .end(function (err) {
+                    if (err) return done(err);
+                    done(err);
+                });
+        });
+
+        it('Exist team - invalid data to update team', function (done) {
+            const data = Object.assign(teams[0], {name: "ChangeName", email: "changeemailmail.com", url: "changeurl.com.br"});
+
+            request(mock)
+                .patch('/teams/'+teams[0]._id)
+                .send(data)
+                .set('Authorization', `JWT ${user.token}`)
+                .expect(422)
+                .end(function (err) {
+                    if (err) return done(err);
+                    done(err);
+                });
+        });
+
+        it('Exist team - try to update team withou token', function (done) {
+            const data = Object.assign(teams[0], {name: "ChangeName", email: "changeemail@email.com", url: "http://changeurl.com.br"});
+
+            request(mock)
+                .patch('/teams/'+teams[0]._id)
+                .send(data)
+                .expect(401)
+                .end(function (err) {
+                    if (err) return done(err);
+                    done(err);
+                });
+        });
+    });
+
+    describe('e2e teams: confirm update team', function () {
+
+        it('Exist team - confirm my changes', function (done) {
+            request(mock)
+                .get('/teams/'+teams[0]._id)
+                .set('Authorization', `JWT ${user.token}`)
+                .expect(200)
+                .expect('Content-Type', /json/)
+                .expect(/ChangeName/)
+                .expect(/changeemail@email\.com/)
+                .expect(/http:\/\/changeurl\.com\.br/)
+                .end(function (err) {
+                    if (err) return done(err);
+                    done(err);
+                });
+        });
+    });
+
+    describe('e2e teams: add members', function () {
+        it('Exist members - valid data to add members', function (done) {
+            const data = {role: "3", id: friend._id, refs: "users"};
+
+            request(mock)
+                .post('/teams/'+teams[0]._id+'/members')
+                .send(data)
+                .set('Authorization', `JWT ${user.token}`)
+                .expect((e) => console.log(e.body))
+                .expect(201)
+                .expect('Content-Type', /json/)
+                .expect(/\"_ref\":\"users\"/)
+                .end(function (err) {
+                    if (err) return done(err);
+                    done(err);
+                });
+        });
+
+        it('Exist members - invalid data to add members (miss role)', function (done) {
+            request(mock)
+                .post('/teams/'+teams[0]._id+'/members')
+                .send(friend)
+                .set('Authorization', `JWT ${user.token}`)
+                .expect(422)
+                .end(function (err) {
+                    if (err) return done(err);
+                    done(err);
+                });
+        });
+
+        it('Exist members - add members withou token', function (done) {
+            const data = {role: "3", id: friend._id, refs: "users"};
+
+            request(mock)
+                .post('/teams/'+teams[0]._id+'/members')
+                .send(data)
+                .expect(401)
+                .end(function (err) {
+                    if (err) return done(err);
+                    done(err);
+                });
+        });
+    });
+
+    describe('e2e teams: get members', function () {
+        it('Exist members - confirm my news members', function (done) {
+            request(mock)
+                .get('/teams/'+teams[0]._id)
+                .set('Authorization', `JWT ${user.token}`)
+                .expect(200)
+                .expect('Content-Type', /json/)
+                .expect(/Friend/)
+                .expect(/\"role\":\"3\"/)
+                .expect(function(res) {
+                    expect(res.body.members).to.have.length(2);
+                })
+                .end(function (err) {
+                    if (err) return done(err);
+                    done(err);
+                });
+        });
+    });
+
+    describe('e2e teams: update members', function () {
+
+    });
+
+    describe('e2e teams: confirm update members', function () {
+
+    });
+
+    describe('e2e teams: delete members', function () {
+
+    });
+
+    describe('e2e teams: confirm delete members', function () {
+
+    });
+
+    describe('e2e teams: delete team', function () {
+
+    });
+
+    describe('e2e teams: confirm to delete team', function () {
+
     });
 
 });
