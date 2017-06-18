ec3562c85aa9a4ebc84ea197d7b9f8158165fb86
Felipe Signorini <felipeklerk@yahoo.com.br>
2017-06-18 00:39:47 -0300

    roles

---
+++ app/core/validators/accessEmpty.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const PermissionError = require('../../errors/permissionError');
+
+module.exports = function (e) {
+
+    return new Promise((resolve) => {
+
+        if (!e)
+            throw new PermissionError('You dont have access');
+
+        resolve(e);
+
+    });
+
+
+};
