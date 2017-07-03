a84400cfdd148ff2a557bc6266f9a4df5674ad6b
Felipe Signorini <felipeklerk@yahoo.com.br>
2017-07-02 22:19:09 -0300

    db

--- app/core/errors/factoryError.js
+++ app/core/errors/factoryError.js
@@ -4,10 +4,8 @@ const FactoryError = (name) => function(errors, msg='error') {
     let err = Error.call(this, msg);
     err.name = name;
     err.errors = errors;
-
-    console.log(this);
     return err;
-}
+};
 
 FactoryError.prototype = Object.create(Error.prototype, {
     constructor: { value: FactoryError }
