c65b82ed81af8f4a918d1823a2bb9f98f7d31708
Felipe Signorini <felipeklerk@yahoo.com.br>
2017-07-03 12:48:38 -0300

    middlewares refactoring

--- app/core/middlewares/errorCatcher.js
+++ app/core/middlewares/errorCatcher.js
@@ -1,7 +1,17 @@
 'use strict';
 
 module.exports = function () {
-    return function (err, req, res) {
-        res.status(500).json({error:err.message});
+
+    const mapError = {
+      NotFoundError: 404,
+      ConflictError: 409,
+      PermissionError: 400,
+      ValidatorError: 422
+    };
+
+    return function (err, req, res, next) {
+        const code = mapError[err.name] || 500;
+        res.status(code).json({err});
+        next();
     };
 };
