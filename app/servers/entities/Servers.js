fc71fd57d126e8842941f45654559b1a2f82f972
Felipe Signorini <felipeklerk@yahoo.com.br>
2017-07-31 19:19:52 -0300

    inventory

--- app/servers/entities/Servers.js
+++ app/servers/entities/Servers.js
@@ -5,12 +5,12 @@ const _ = require('lodash');
 const Servers = require('../repositories/dao/db');
 
 const servers = () => {
-    const resFilled = ['_id', 'hostname', 'ipv4_private', 'ipv4_public', 'os.base', 'os.dist', 'dc.name', 'dc.zone', 'role', 'environment', 'auth.name', 'auth.admin', 'updated_at', 'created_at'];
+    const resFilled = ['_id', 'updated_at', 'created_at', 'status', 'hostname', 'ipv4_private', 'ipv4_public', 'os.base', 'os.dist', 'dc.name', 'dc.zone', 'role', 'environment', 'auth.name', 'auth.admin', 'tags'];
 
-    const singleFilled = ['cpu', 'memory', 'storage', 'services', 'dc', 'os', 'auth', 'role', 'environment', 'meta',
-    'roles', 'owner', ...resFilled];
+    const singleFilled = [...resFilled, 'cpu', 'memory', 'storage', 'services', 'dc', 'os', 'auth', 'role', 'environment', 'meta',
+    'roles', 'owner'];
 
-    const filled = [..._.tail(singleFilled)]; // delete id
+    const filled = [..._.slice(singleFilled, 3)]; // delete id
 
     return {
       name: "servers",
--- app/servers/validators/index.js
+++ app/servers/validators/index.js
@@ -2,26 +2,56 @@
 
 const Joi = require('joi');
 
+const storage = Joi.object().keys({
+  name: Joi.string(),
+  size: Joi.number(),
+  root: Joi.string()
+});
+
+const services = Joi.object().keys({
+  name: Joi.string(),
+  version: Joi.string(),
+  implements: Joi.object()
+});
+
+const auth = Joi.object().keys({
+  name: Joi.string(),
+  type: Joi.string().valid('PKI', 'AD', 'LDAP', 'Password'),
+  username: Joi.string(),
+  key: Joi.string()
+});
+
+const tags = Joi.object().keys({
+  value: Joi.string(),
+  key: Joi.string()
+});
+
 const schema = Joi.object().keys({
+    status: Joi.string().valid('Active', 'Avaliable', 'Deprecated'),
     hostname: Joi.string().required(),
     ipv4_private: Joi.string().ip().required(),
     ipv4_public: Joi.string().ip(),
     os: Joi.object({
       base: Joi.string().required(),
-      dist: Joi.any(),
-      version: Joi.any()
+      dist: Joi.string(),
+      version: Joi.string()
     }).required(),
     cpu: Joi.number(),
     memory: Joi.number(),
-    storage: Joi.array(),
-    services: Joi.array(),
+    storage: Joi.array().items(storage),
+    services: Joi.array().items(services),
     dc: Joi.object(),
     role: Joi.string().valid('Application', 'Container', 'Database', 'Hybrid').required(),
     environment: Joi.string().valid('Production', 'Staging', 'Development', 'UTA', 'Training').required(),
-    auth: Joi.array(),
+    auth: Joi.array().items(auth),
+    tags: Joi.array().items(tags),
     meta: Joi.any(),
-    owner: Joi.any(),
-    roles: Joi.any(),
+    owner: Joi.object({
+      email: Joi.string().email(),
+      _id: Joi.object(),
+      refs: Joi.string()
+    }),
+    roles: Joi.array(),
     active: Joi.boolean()
 });
 
