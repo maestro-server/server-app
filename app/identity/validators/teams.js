c599ddaed11d1e85a026fe882082cc17899208dd
Felipe Klerk Signorini <felipe.signorini@b2wdigital.com>
2017-08-30 19:45:00 -0300

    user - pu to patch

--- app/identity/validators/teams.js
+++ app/identity/validators/teams.js
@@ -2,13 +2,29 @@
 
 const Joi = require('joi');
 
+const member = Joi.object({
+  email: Joi.string().email(),
+  name: Joi.string().min(1).max(40),
+  _id: Joi.object(),
+  refs: Joi.string().max(35)
+});
+
+const roles = Joi.object().keys({
+  _id: Joi.object(),
+  role: Joi.number().valid(1, 3, 7).required(),
+  refs: Joi.string().valid("users", "teams", "projects").required(),
+  name: Joi.string().max(100),
+  email: Joi.string().max(250)
+});
+
 const schema = Joi.object().keys({
-    owner: Joi.any(),
-    members: Joi.any(),
-    active: Joi.boolean(),
+    owner: member,
+    members: Joi.array().items(roles).unique('_id'),
     name: Joi.string().min(3).max(30).required(),
     email: Joi.string().email(),
-    url: Joi.string().uri({scheme: ['http', 'https']})
+    avatar: Joi.string().min(3).max(200),
+    url: Joi.string().uri({scheme: ['http', 'https']}),
+    active: Joi.boolean()
 });
 
 module.exports = {
