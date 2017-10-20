0d8c714710fd749e2d4a866006b2fc7954ded61e
Felipe Klerk Signorini <felipe.signorini@b2wdigital.com>
2017-10-20 18:33:28 -0200

    docker package process

--- app/identity/entities/Teams.js
+++ app/identity/entities/Teams.js
@@ -5,7 +5,7 @@ const _ = require('lodash');
 const Teams = require('../repositories/dao/teams');
 
 const teams = () => {
-    const resFilled = ['_id', 'updated_at', 'created_at', 'name', 'email', 'url', 'avatar', 'members', 'owner'];
+    const resFilled = ['_id', 'updated_at', 'name', 'email', 'url', 'avatar', 'members', 'owner'];
 
     const singleFilled = [...resFilled];
 
