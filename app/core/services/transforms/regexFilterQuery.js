2670edd43bb448acb3584eed85c6e627c32fd7d1
Felipe Klerk Signorini <felipe.signorini@b2wdigital.com>
2017-10-24 18:05:21 -0200

    search query - case insentive

--- app/core/services/transforms/regexFilterQuery.js
+++ app/core/services/transforms/regexFilterQuery.js
@@ -6,7 +6,7 @@ module.exports = function (query) {
     const filter = _.pickBy(query, _.identity);
 
     const result =  _.map(filter,
-      (o, k) => ({[k]: {$regex: o}})
+      (o, k) => ({[k]: {$regex: o, '$options': 'i'}})
     );
 
     return _.isEmpty(result) ? [] : result;
