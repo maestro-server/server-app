d8780571d150edfbc895d0231a534a5d185d4162
Felipe Klerk Signorini <felipe.signorini@b2wdigital.com>
2017-10-24 18:05:21 -0200

    search query - case insentive

--- migrations/0015-applications_options.js
+++ migrations/0015-applications_options.js
@@ -7,7 +7,7 @@ exports.up = function (db, next) {
         "value": {
             families: ['Application', 'Loadbalance', 'Broker', 'Database', 'Serverless', 'ApiGateway', 'ContainerOrchestration', 'Cache', 'CDN', 'Object Storage', 'Monitoring', 'Logs', 'SMTP', 'ServiceDiscovery', 'VPN', 'Repository', 'DNS', 'CI/CD', 'Auth', 'NAS'],
             role: ['Application', 'Worker', 'Jobs', 'Testing', 'Standard'],
-            deploy: ['Git (Github, Bibucket)', 'Continuos Integration (CI)', 'Continuos Deployment (CD)', 'FTP', 'SFTP'],
+            deploy: ['Git (Github, Bibucket)', 'Continuos Integration (CI)', 'Continuos Deployment (CD)', 'FTP', 'SFTP', 'Others'],
             clusters: ['No', 'Master/Slave', '12 Factor', 'ZooKeeper', 'Leader election'],
         },
         "key": "application_options",
