4f017f5db1703d09264309616b2068b9de135926
Felipe Signorini <felipeklerk@yahoo.com.br>
2017-11-13 00:18:55 -0200

    add entity and family in providers migration

--- migrations/0050-providers_options.js
+++ migrations/0050-providers_options.js
@@ -8,22 +8,82 @@ exports.up = function (db, next) {
             'permissions': {
                 'AWS': {
                     'server-list': [
-                        {'access': 'describe_instances', 'command': 'ec2'}
+                        {
+                            'access': 'describe_instances',
+                            'command': 'ec2',
+                            'entity': 'servers',
+                            'vars': {
+                                'pag': 'MaxResults'
+                            }
+                        }
                     ],
                     'loadbalance-list': [
-                        {'access': 'describe_load_balancers', 'command': 'elbv2'},
-                        {'access': 'describe_target_groups', 'command': 'elbv2'},
-                        {'access': 'describe_target_health', 'command': 'elbv2'},
-                        {'access': 'describe_auto_scaling_groups', 'command': 'elbv2'}
+                        {
+                            'access': 'describe_load_balancers',
+                            'command': 'elbv2',
+                            'entity': 'applications',
+                            'family': 'Loadbalances',
+                            'vars': {
+                                'pag': 'PageSize'
+                            }
+                        },
+                        {
+                            'access': 'describe_target_groups',
+                            'command': 'elbv2',
+                            'entity': 'applications',
+                            'family': 'Loadbalances',
+                            'vars': {
+                                'pag': 'PageSize'
+                            }
+                        },
+                        {
+                            'access': 'describe_target_health',
+                            'command': 'elbv2',
+                            'entity': 'applications',
+                            'family': 'Loadbalances',
+                            'vars': {
+                                'pag': 'PageSize'
+                            }
+                        },
+                        {
+                            'access': 'describe_auto_scaling_groups',
+                            'command': 'elbv2',
+                            'entity': 'applications',
+                            'family': 'Loadbalances',
+                            'vars': {
+                                'pag': 'PageSize'
+                            }
+                        }
                     ],
                     'dbs-lists': [
-                        {'access': 'describe_db_instances', 'command': 'rds'}
+                        {
+                            'access': 'describe_db_instances',
+                            'command': 'rds',
+                            'entity': 'applications',
+                            'family': 'Databases',
+                            'vars': {
+                                'pag': 'MaxRecords'
+                            }
+                        }
                     ],
                     'storage-object-list': [
-                        {'access': 'list_buckets', 'command': 's3api'}
+                        {
+                            'access': 'list_buckets',
+                            'command': 's3api',
+                            'entity': 'applications',
+                            'family': 'ObjectStorage'
+                        }
                     ],
                     'cdns-list': [
-                        {'access': 'list_distributions', 'command': 'cloudfront'}
+                        {
+                            'access': 'list_distributions',
+                            'command': 'cloudfront',
+                            'entity': 'applications',
+                            'family': 'CDN',
+                            'vars': {
+                                'pag': 'MaxItems'
+                            }
+                        }
                     ]
                 }
             }
