84e17b7d47c191eab0b32539089197ac6084f92d
Felipe Klerk Signorini <felipe.signorini@b2wdigital.com>
2017-11-21 19:35:59 -0200

    create config file

--- app/core/routers/index.js
+++ app/core/routers/index.js
@@ -6,11 +6,16 @@ module.exports = function (router) {
 
     router.get('/', function (req, res) {
 
+      const static_url = process.env.FRONT_STATIC_URL || '/upload/';
+      const api_timeout = process.env.FRONT_TIMEOUT || 8000;
+
       res.json(
         {
           app,
           description,
-          version
+          version,
+          static_url,
+          api_timeout
         });
     });
 };
