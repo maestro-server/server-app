bd0e2689881089169a7e978e0cfee430b7300ffa
Felipe Klerk Signorini <felipe.signorini@b2wdigital.com>
2017-12-04 20:43:17 -0200

    ajust, - dc entities

--- app/inventory/entities/Snapshots.js
+++ app/inventory/entities/Snapshots.js
@@ -6,10 +6,10 @@ const Snapshots = require('../repositories/dao/snapshots');
 
 const snapshots = () => {
     const resFilled = ['_id', 'updated_at', 'created_at', 'name', 'datacenters', 'volume_id', 'volume_size',
-        'state', 'snapshot_id', 'progress'];
+        'status', 'snapshot_id', 'progress'];
 
     const singleFilled = [...resFilled, 'kms_key_id', 'encrypted', 'description',
-        'data_encryption_key_id', 'owner_id', 'start_time', 'owner_alias', 'state_message'];
+        'data_encryption_key_id', 'owner_id', 'start_time', 'owner_alias', 'state_message', 'roles'];
 
     const filled = [..._.slice(singleFilled, 3)];  // delete id
 
