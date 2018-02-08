f011af12800998cab01d1bfa6c102094fdf77ad8
Felipe Signorini <fsignorini@rdisoftware.com>
2018-02-08 23:21:09 +0000

    validate reports

--- app/reports/routers/reports/index.js
+++ app/reports/routers/reports/index.js
@@ -5,23 +5,25 @@ const authenticate = require('identity/middlewares/authenticate');
 const Reports = require('../../entities/Report');
 const Team = require('identity/entities/Teams');
 
-const WrapperPersistenceApp = require('core/applications/wrapperPersistenceApplication')(Reports)(Team)();
+const PersistenceReport = require('../../applications/persistenceReports');
+const WrapperPersistenceApp = require('core/applications/wrapperPersistenceApplication')(Reports)(Team);
+const WrapperPersistenceAppReports = WrapperPersistenceApp(PersistenceReport);
 
 const AccessApp = require('core/applications/accessApplication');
-const WrapperAccessApp = WrapperPersistenceApp(AccessApp);
+const WrapperAccessApp = WrapperPersistenceApp()(AccessApp);
 
 module.exports = function (router) {
 
     router
-        .get('/teams/:id/reports', authenticate(), WrapperPersistenceApp().find)
+        .get('/teams/:id/reports', authenticate(), WrapperPersistenceApp()().find)
 
-        .get('/teams/:id/reports/count', authenticate(), WrapperPersistenceApp().count)
+        .get('/teams/:id/reports/count', authenticate(), WrapperPersistenceApp()().count)
 
-        .get('/teams/:id/reports/:idu', authenticate(), WrapperPersistenceApp().findOne)
+        .get('/teams/:id/reports/:idu', authenticate(), WrapperPersistenceApp()().findOne)
 
-        .put('/teams/:id/reports/:idu', authenticate(), WrapperPersistenceApp().update)
+        .put('/teams/:id/reports/:idu', authenticate(), WrapperPersistenceApp()().update)
 
-        .patch('/teams/:id/reports/:idu', authenticate(), WrapperPersistenceApp().patch)
+        .patch('/teams/:id/reports/:idu', authenticate(), WrapperPersistenceApp()().patch)
 
         /**
          * @api {delete} /teams/:id/applications/:idu Delete application of team
@@ -40,9 +42,9 @@ module.exports = function (router) {
          * @apiSuccessExample {json} Success-Response:
          *     HTTP/1.1 204 OK
          */
-        .delete('/teams/:id/reports/:idu', authenticate(), WrapperPersistenceApp().remove)
+        .delete('/teams/:id/reports/:idu', authenticate(), WrapperPersistenceAppReports().remove)
 
-        .post('/teams/:id/reports', authenticate(), WrapperPersistenceApp().create)
+        .post('/teams/:id/reports', authenticate(), WrapperPersistenceAppReports().create)
 
         /**
          * Roles
