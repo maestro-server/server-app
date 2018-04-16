821499429357616683a6d238cd50619b66dcc018
fsignorini <fsignorini@rdisoftware.com>
2018-04-16 23:09:29 +0000

    scheduler validate

--- app/playbooks/validators/scheduler.js
+++ app/playbooks/validators/scheduler.js
@@ -2,14 +2,26 @@
 
 const Joi = require('joi');
 
-const {roles, owner} = require('core/validators/validators');
+const {created_at, active, roles, owner, interval, crontab, chain} = require('core/validators/validators');
 
 const schema = Joi.object().keys({
+    _cls: Joi.string().default('PeriodicTask'),
     name: Joi.string().min(3).max(30).required(),
+    enabled: Joi.boolean().default(true),
+    interval: Joi.array().items(interval),
+    crontab: Joi.array().items(crontab),
+    args: Joi.array().items(Joi.string()),
+    kwargs: Joi.object(),
+    chain: Joi.array().items(chain),
+    endpoint: Joi.string().uri(),
+    method: Joi.string().valid('GET', 'POST', 'PUT', 'DELETE').default('GET'),
+    total_run_count: Joi.number().default(1),
+    max_execution: Joi.number(),
     roles: Joi.array().items(roles).unique('_id'),
     owner,
-    active: Joi.boolean(),
-    created_at: Joi.any()
+    active,
+    created_at,
+    last_run_at: Joi.any()
 });
 
 module.exports = {
