3f95c170bf1be8df485f0fdc89c2e920e64dfb60
fsignorini <fsignorini@rdisoftware.com>
2018-04-20 21:39:00 +0000

    template - scheduler

--- app/playbooks/applications/persistenceScheduler.js
+++ app/playbooks/applications/persistenceScheduler.js
@@ -6,26 +6,36 @@ const DPersistenceServices = require('core/services/PersistenceServices');
 const PersistenceApplication = require('core/applications/persistenceApplication');
 
 const jsonParser = require('core/applications/transforms/jsonParser');
-const filterHooks = require('inventory/applications/transforms/filterHooks');
+const filterHooks = require('core/applications/transforms/filterHooks');
+const TemplateScheduler = require('../services/templateScheduler');
 
 const ApplicationSchedulers = (Entity, PersistenceServices = DPersistenceServices) => {
 
-    return {
-      find(req, res, next) {
-          const field = 'query';
-          let query = _.clone(req.query);
+  return {
+    find(req, res, next) {
+      const field = 'query';
+      let query = _.clone(req.query);
 
-          query = jsonParser(query, field);
+      query = jsonParser(query, field);
 
-          query = filterHooks(query, field, [
-            {dest: 'link.refs', source: 'modules', module: 'swap'}
-          ]);
+      query = filterHooks(query, field, [
+        { dest: 'link.refs', source: 'modules', module: 'swap' }
+      ]);
 
-          Object.assign(req, {query});
-          PersistenceApplication(Entity, PersistenceServices)
-            .find(req, res, next);
-      }
-    };
+      Object.assign(req, { query });
+      PersistenceApplication(Entity, PersistenceServices)
+        .find(req, res, next);
+    },
+
+    createTemplate(req, res, next) {
+      const template = TemplateScheduler(req).template(req.body);
+      _.set(req, 'body', template);
+      console.log(template);
+
+      PersistenceApplication(Entity, PersistenceServices)
+        .create(req, res, next);
+    }
+  };
 };
 
 module.exports = _.curry(ApplicationSchedulers);
