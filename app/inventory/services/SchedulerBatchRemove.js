a70f31efca8b4b67593db038aa75de70f92a177e
Felipe Signorini <felipeklerk@yahoo.com.br>
2018-08-12 20:43:32 -0300

    subs - playbooks for analytics

--- app/inventory/services/SchedulerBatchRemove.js
+++ app/inventory/services/SchedulerBatchRemove.js
@@ -3,7 +3,7 @@
 const Access = require('core/entities/accessRole');
 const DFactoryDBRepository = require('core/repositories/DBRepository');
 const accessMergeTransform = require('core/services/transforms/accessMergeTransform');
-const Scheduler = require('playbooks/entities/Scheduler');
+const Scheduler = require('reports/entities/Scheduler');
 const in_maker = require('core/libs/in_maker');
 
 const SchedulerBatch = (req) => (PersistenceServices, FactoryDBRepository = DFactoryDBRepository) => {
