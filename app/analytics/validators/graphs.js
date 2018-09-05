c45803f688b05e509659c09bfd6cf5bbfbe162a6
fsignorini <fsignorini@rdisoftware.com>
2018-09-05 22:37:03 +0000

    public graph-

--- app/analytics/validators/graphs.js
+++ app/analytics/validators/graphs.js
@@ -13,6 +13,29 @@ const schema = Joi.object().keys({
     systems: Joi.array().items(entries).unique(),
     apps: Joi.array().items(entries).unique(),
     info: Joi.any(),
+    spublic: Joi.boolean(),
+    iclients: Joi.any(),
+    iservers: Joi.any(),
+    isystems: Joi.any(),
+    ifamilies: Joi.any(),
+    msg: Joi.string(),
+    filters: Joi.any(),
+    status: Joi.string().valid('process', 'finished', 'error', 'warning').default('process'),
+    owner,
+    active: Joi.boolean(),
+    created_at: Joi.any()
+});
+
+const update = Joi.object().keys({
+    name: Joi.string().min(3).max(30),
+    roles: Joi.array().items(roles).unique('_id'),
+    type: Joi.string().valid('bussiness', 'infra'),
+    tab: Joi.number().min(0).max(3).default(0),
+    clients: Joi.array().items(entries).unique(),
+    systems: Joi.array().items(entries).unique(),
+    apps: Joi.array().items(entries).unique(),
+    info: Joi.any(),
+    spublic: Joi.boolean().default(true),
     iclients: Joi.any(),
     iservers: Joi.any(),
     isystems: Joi.any(),
@@ -27,7 +50,7 @@ const schema = Joi.object().keys({
 
 module.exports = {
     create: schema,
-    update: schema,
+    update: update,
     delete: {},
     list: {}
 };
