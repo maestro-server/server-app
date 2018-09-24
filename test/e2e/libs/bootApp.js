bcedc8f9e7d9dcf11648924ecf71150ef8c56335
fsignorini <fsignorini@rdisoftware.com>
2018-09-24 17:57:37 +0000

    change connection string - complience

--- test/e2e/libs/bootApp.js
+++ test/e2e/libs/bootApp.js
@@ -7,11 +7,11 @@ const kraken = require('kraken-js');
 
 const db_connect = require('core/libs/db_run');
 const Mongorito = require('mongorito');
-
+const dbpath = require('core/libs/dbpath')();
 const path = require('path');
 
 
-module.exports = function (conn = process.env.MAESTRO_MONGO_URI) {
+module.exports = function (conn = dbpath) {
 
     let options = {
         basedir: path.resolve(__dirname, '../../../app/'),
