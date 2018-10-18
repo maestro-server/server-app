84f2bab85611301e9ce885debab71eafa238a16e
Felipe Signorini <felipeklerk@yahoo.com.br>
2018-10-18 20:00:20 -0300

    server entity

--- app/inventory/entities/Servers.js
+++ app/inventory/entities/Servers.js
@@ -5,7 +5,10 @@ const _ = require('lodash');
 const Servers = require('../repositories/dao/servers');
 
 const servers = () => {
-    const resFilled = ['_id', 'updated_at', 'created_at', 'hostname', 'ipv4_private', 'ipv4_public', 'os.base', 'os.dist', 'datacenters.name', 'datacenters.region', 'datacenters.zone', 'datacenters._id', 'datacenters.instance_id', 'role', 'environment', 'auth.name', 'auth.username', 'auth.type', 'services', 'tags'];
+    const resFilled = ['_id', 'updated_at', 'created_at', 'hostname',
+        'ipv4_private', 'ipv4_public', 'os.base', 'os.dist',
+        'datacenters.name', 'datacenters.region', 'datacenters.zone', 'datacenters._id', 'datacenters.instance_id',
+        'role', 'environment', 'auth.name', 'auth.username', 'auth.type', 'services', 'tags'];
 
     const singleFilled = [...resFilled, 'unique_id', 'cpu', 'memory', 'storage', 'datacenters', 'os', 'auth', 'role', 'environment',
         'roles', 'owner', 'active', 'status', 'meta'];
