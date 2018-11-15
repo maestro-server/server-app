bfdee7414dc444e69e3f37b8c73f7e193368f88d
Felipe Signorini <felipeklerk@yahoo.com.br>
2018-11-15 12:18:55 -0200

    add http conn

--- migrations/0080-deps_options.1.js
+++ migrations/0080-deps_options.1.js
@@ -5,7 +5,7 @@ exports.up = function (db, next) {
     let pets = db.collection('adminer');
     pets.insert({
         "value": {
-            protocol: ['rest', 'graphQL', 'falcor', 'thrift', 'soap', 'xml-rpc',
+            protocol: ['rest', 'http', 'graphQL', 'falcor', 'thrift', 'soap', 'xml-rpc',
                 'gRPC', 'gossip', 'beep', 'cts', 'wps', 'wscl', 'wsfl', 'wcf', 'xins'],
             protocol_broker: ['amqp', 'mqtt', 'streams', 'tcp/ip'],
             protocol_default: ['tcp/ip', 'udp', 'outhers']
