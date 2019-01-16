3e0744e6c07d92bd853204a09cd64725e9706cb2
Felipe Signorini <felipeklerk@yahoo.com.br>
2019-01-15 23:07:55 -0200

    connection default

--- app/core/libs/dbpath.js
+++ app/core/libs/dbpath.js
@@ -2,7 +2,7 @@
 
 
 module.exports = () => {
-    const uri = process.env.MAESTRO_MONGO_URI || 'localhost';
+    const uri = process.env.MAESTRO_MONGO_URI || 'mongodb://localhost';
     const db = process.env.MAESTRO_MONGO_DATABASE || 'maestro-client';
     return `${uri}/${db}`;
 };
