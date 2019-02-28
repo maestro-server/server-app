d91a554a885d5db58d27e602c8efb126808a276d
Felipe Signorini <felipeklerk@yahoo.com.br>
2019-02-28 09:37:08 +0000

    dc and zones - devtools migration

---
+++ devtools/migration_demo/data/deps.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const json = {
+    "tree": {
+        "root": [{
+            "_id": "5c77247ba87dac303c0d1216",
+            "name": "WebClient SPA Maestro",
+            "family": "Application",
+            "environment": "Production",
+            "endpoint": null
+        }],
+        "5c77247ba87dac303c0d1216": [{
+            "_id": "5c77247ba87dac303c0d1215",
+            "name": "WebServer Maestro",
+            "family": "Application",
+            "environment": "Production",
+            "endpoint": "rest"
+        }],
+        "5c77247ba87dac303c0d1215": [{
+            "_id": "5c77247ba87dac303c0d1214",
+            "name": "Discovery API",
+            "family": "Application",
+            "environment": "Production",
+            "endpoint": "rest"
+        }]
+    },
+    "systems": [{"_id": "5c772473a87dac303c0d10a9", "name": "Maestro - Client"}, {
+        "_id": "5c772473a87dac303c0d10a8",
+        "name": "Maestro - Servers"
+    }]
+};
+
+module.exports = regizons;
